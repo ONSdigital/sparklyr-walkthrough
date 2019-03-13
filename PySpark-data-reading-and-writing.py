e22aa7bd6a111ec7ada6aa57219c20d73e1ab6a0
cmusselle <cmusselle@mango-solutions.com>
2019-03-13 09:49:45 +0000

    Add scripts on data mainpulation

---
+++ PySpark-data-reading-and-writing.py
@@ -0,0 +1,13 @@
+## Import the necessary Packages to work with Spark
+
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+import pandas as pd
+
+# Data exists in multiple locations 
+
+
+
+
+## Load in some sample data 
+
